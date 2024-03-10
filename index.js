5c9896211a90ef02edc7558c7cd355920cbdce7a
Shubh <shubh.scb@gmail.com>
2024-03-10 22:34:02 +0530

    bug: supertokens import bug

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ import bodyParser from "body-parser";
 import userRoutes from "./src/routes/userRoutes.js";
 import candidateRoutes from "./src/routes/candidateRoutes.js";
 import cors from "cors";
-import SuperTokens from "supertokens-node";
+import supertokens from "supertokens-node";
+// import { middleware, errorHandler, SessionRequest } from "supertokens-node/lib/build/framework/express/framework.js";
+import {
+  middleware,
+  errorHandler,
+  SessionRequest,
+} from "supertokens-node/framework/express";
+// import { getWebsiteDomain, SuperTokensConfig } from "./src/config/supertokensConfig.js";
+import { getWebsiteDomain, SuperTokensConfig } from "./config";
 // import { Dropbox } from "dropbox";
 
 dotenv.config();
 const accessToken = process.env.DROPBOX_ACCESS_TOKEN;
+
 
 // const dbx = new Dropbox({ accessToken, fetch });
 // let directLink = "";
@@ -37,12 +46,31 @@ const accessToken = process.env.DROPBOX_ACCESS_TOKEN;
 //   }
 // };
 
+supertokens.init(SuperTokensConfig);
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: getWebsiteDomain(),
+  allowedHeaders: ["Content-Type", ...supertokens.getAllCORSHeaders()],
+  methods: ["GET", "POST", "DELETE", "PUT"],
+  credentials: true,
+}));
 app.use(bodyParser.json());
+app.use(middleware);
 
+//  for testing
+app.get("/sessioninfo", verifySession(), async (req, res) => {
+  let session = req.session;
+  req.send({
+    sessionHandle: session.getHandle(),
+    userId: session.getUserId(),
+    accessTokenPayload: session.getAccessTokenPayload(),
+  });
+  })
+
+
+// want to implement supertokens here
 app.use("/users", userRoutes);
-app.use("/candidates", candidateRoutes);
+app.use("/candidates", verifySession(), candidateRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello, this is the root route!");
@@ -52,6 +80,7 @@ const PORT = process.env.PORT || 3000;
 // getDropboxDirectLink();
 
 console.log("PORT", PORT);
+app.use(errorHandler());
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
